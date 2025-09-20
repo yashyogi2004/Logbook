305d6cb40885afc560e3fe40b00a0dc299802511
yashyogi2004 <yashyog2004@gmail.com>
2025-09-20 21:20:28 +0530

    .env

--- server/Routes/AuthRoutes.js
+++ server/Routes/AuthRoutes.js
@@ -48,7 +48,9 @@ router.post('/login', async (req, res) => {
         res.cookie('token', token, {
             httpOnly: true,
             secure: true,
-            sameSite: 'Lax', maxAge: 3600000 });
+            sameSite: 'None',
+            maxAge: 24 * 60 * 60 * 1000 // 1 day
+        });
         res.status(200).json({ message: 'Login successful' });
 
     } catch (error) {
