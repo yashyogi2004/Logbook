55415d8522cbf27c68e0f56b40a20dc3c682df4e
yashyogi2004 <yashyog2004@gmail.com>
2025-12-22 11:39:49 +0530

    port changes in smtp

--- server/controller/transporter.js
+++ server/controller/transporter.js
@@ -5,8 +5,8 @@ dotenv.config();
 // Create a test account or replace with real credentials.
 const transporter = nodemailer.createTransport({
   host: process.env.host,
-  port: 465,
-  secure: true, // true for 465, false for other ports
+  port: 587,
+  secure: false, // true for 465, false for other ports
   auth: {
     user: process.env.user,
     pass: process.env.pass,
